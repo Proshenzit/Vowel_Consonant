81e8e0165e008f4ac5ca456de647688ad2d988d3
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-05-15 20:37:01 -0700

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,30 @@
+
+
+var letter=prompt("Enter your letter: ");
+
+letter=letter.toLocaleLowerCase();
+
+if(letter=="a" || letter=="e" || letter=="i" ||letter=="o"  || letter=="u")
+
+console.log("vowel");
+
+else 
+
+console.log("consonant");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
